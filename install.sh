9fa5ab00a8371b42d391935ff18e73733b007169
David Nuon <david@davidnuon.com>
2016-02-18 02:37:11 -0800

    Corrected provisioning script add key typo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-key -y adv  --keyserver keyserver.ubuntu.com --recv-keys 575159689BEFB442 --force-yes
+sudo apt-key adv  --keyserver keyserver.ubuntu.com --recv-keys 575159689BEFB442 --force-yes
 echo 'deb http://download.fpcomplete.com/ubuntu trusty main'| sudo tee /etc/apt/sources.list.d/fpco.list
 sudo apt-get update
 
