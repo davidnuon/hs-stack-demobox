1012ec04fe0f5d8a527db33f8cc7bc49e58d2fc5
karim amer <karim.amer.sa@gmail.com>
2016-02-18 18:47:55 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 575159689BEFB442
 echo 'deb http://download.fpcomplete.com/ubuntu trusty main'|sudo tee /etc/apt/sources.list.d/fpco.list
 sudo apt-get update
 
-sudo apt-get -y install postgresql postgresql-contrib
+
 sudo apt-get install -y haskell-platform
 sudo apt-get install -y stack
 sudo apt-get install -y git
